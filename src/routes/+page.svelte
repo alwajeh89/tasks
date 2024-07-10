ce9358f74c5834bffd7530ea58a680412752c746
AAA <alwajeh89@gmail.com>
2024-07-10 21:50:14 +0300

    cc

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,7 @@
 <script lang="ts">
-
-import dayjs from "dayjs";
-import 'dayjs/locale/ar'
-
-dayjs.locale('ar');
-
-let period = dayjs().format('a')=="م"?'مساء':'صباح';
+	import Header from './../lib/header.svelte';
 
 </script>
-
 <div>
-<header class="flex justify-between p-12">
-
-	<div>
-<h1 class=" text-4xl">{period} الخير</h1>
-<h3 class="text-2xl text-surface-900/40">{dayjs().format('اليوم ، dddd D MMM YYYY')}</h3>
-	</div>
-	<div class="bg-gray-500 h-16 w-32 ">
-
-	</div>
-</header>
+<Header></Header>
 </div>
